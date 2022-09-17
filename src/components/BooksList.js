66d0b0e655f8ad34bc25473fe30d8c58aafb9c1b
Mirwais Farahi <waisfarahi@gmail.com>
2022-09-17 15:53:43 +0500

    Add File

---
+++ src/components/BooksList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AddBook from './AddBook';
+import BookCard from './BookCard';
+
+function BooksList() {
+  const retrieveBook = <BookCard title="Java Programming" author="Andrew High" />;
+
+  return (
+    <div>
+      <h1>Books</h1>
+      {retrieveBook}
+      <AddBook />
+    </div>
+  );
+}
+
+export default BooksList;
