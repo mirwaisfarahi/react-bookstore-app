d9b002a3e4f5c3cef79e16696faa63292513081f
Mirwais Farahi <waisfarahi@gmail.com>
2022-09-17 15:54:23 +0500

    Edit AddBook.js File

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const AddBook = () => (
-  <form>
+const AddBook = () => {
+  
+  return (
+    <form>
     <input type="text" placeholder="title" />
     <input type="text" placeholder="author" />
     <button type="submit">Add Book</button>
   </form>
-);
+  );
+  
+};
 
 export default AddBook;
