57789674014de610b28630d620182f5e502b2ca6
Mirwais Farahi <waisfarahi@gmail.com>
2022-09-22 01:02:51 +0500

    Edit configureStore.js File

---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/books';
+import categoriesReducer from './categories/categories';
+
+const rootReducer = combineReducers({
+  books: booksReducer,
+  categories: categoriesReducer,
+});
+
+const store = configureStore({
+  rootReducer,
+});
+
+export default store;
