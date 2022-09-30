d4a1dfe6da03cc1c19074e9b44dd9075e83d213e
Mirwais Farahi <waisfarahi@gmail.com>
2022-09-30 22:23:56 +0500

    Edit categories.js File

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,18 +1,27 @@
-const CHECK_STATUS = 'bookStore/categories/CHECK_STATUS';
+const BOOKLIST = 'catagories/catagories/BOOKLIST';
 
-const categories = [];
+const categories = {
+  message: '',
+};
 
-// Reducer
-const categoriesReducer = (state = categories, action) => {
+export const checkBook = () => (dispatch) => {
+  dispatch({
+    type: BOOKLIST,
+    payload: 'Under construction',
+  });
+};
+
+const categoryReducer = (state = categories, action) => {
   switch (action.type) {
-    case CHECK_STATUS:
-      return 'Under Contruction';
+    case BOOKLIST:
+      return {
+        ...state,
+        message: [...state.message, action.payload],
+      };
+
     default:
       return state;
   }
 };
 
-// ActionsTypes
-export const checkStatus = () => ({ type: CHECK_STATUS });
-
-export default categoriesReducer;
+export default categoryReducer;
