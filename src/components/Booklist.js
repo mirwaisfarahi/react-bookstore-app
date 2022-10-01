e2819f123c20d20109521e58feef60622155819b
Mirwais Farahi <waisfarahi@gmail.com>
2022-10-01 17:08:16 +0500

    Edit Booklist.js File

---
+++ src/components/Booklist.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import uuid from 'react-uuid';
+import Book from './Book';
+import Form from './Form';
+
+import '../assets/Styles/booklist.css';
+import { showBook } from '../redux/books/books';
+
+const Booklist = () => {
+  const dispatch = useDispatch();
+  const { bookArray } = useSelector((state) => state.book);
+  useEffect(() => {
+    dispatch(showBook());
+  }, []);
+
+  return (
+    <div className="book">
+      {bookArray.map((book) => (
+        <Book
+          key={uuid()}
+          title={book.title}
+          author={book.author}
+          category={book.category}
+          id={book.item_id}
+        />
+      ))}
+      <div>
+        <Form />
+      </div>
+    </div>
+  );
+};
+
+export default Booklist;
